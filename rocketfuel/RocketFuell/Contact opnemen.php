c8dfa7fd514d494202d730f3a7b2a830570e7ac8
Zakariyaq <74624683+Zakariyaq@users.noreply.github.com>
2021-03-19 12:56:15 +0100

    Add files via upload

---
+++ rocketfuel/RocketFuell/Contact opnemen.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale">
+ <title>Contact opnemen</title>
+</head>
+
+<body>
+   <header>
+       <img src="foto/logo.png" alt="logo">
+       <nav id="taakbalk">
+           <ul>
+        <li>Home</li>
+        <li>Aanbiedingen</li>
+        <li>Onze Producten</li>
+        <li>De nieuwe opkomende smaken</li>
+        <li>FAQ</li>
+        <li>Contact opnemen</li>
+</ul>
+</nav>
+    </header>
+   <main class="kern">
+    
+    
+</body>
+</html>
+
+
+
+   
