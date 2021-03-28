a773e05f5f65ce462748ccc447914f48737d1dec
Zakariya issa <116040@mborijnland.nl>
2021-03-28 18:44:55 +0200

    new

--- rocketfuel/Contact opnemen.php
+++ rocketfuel/Contact opnemen.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale">
  <title>Contact opnemen</title>
- <link rel="stylesheet" type="text/css" href="CSS/contact opnemen style.css">
+ 
 </head>
 <?php
 if(isset($_GET['voornaam']))
@@ -14,6 +14,17 @@ if(isset($_GET['voornaam']))
     echo $_GET['voornaam']."<br/>"
 }
 ?>
+
+<h2>
+    als je vragen hebt of meer informatie nodig hebt.
+    je kan ons contacteren via die formulier hieronder
+</h2>
+ <p> Contact opnemen:____________________________;</p>
+
+    
+   
+</body>
+<body>
 
 
 
